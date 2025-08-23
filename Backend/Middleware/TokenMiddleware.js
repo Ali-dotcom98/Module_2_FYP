97622aa5a7b51e085ac3a5e7f00fa0184d659eeb
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-08-23 09:57:41 +0500

    Middleware Added

---
+++ Backend/Middleware/TokenMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const SecretKey = process.env.SECRET_KEY;
+
+
+const GenerateToken = (Payload) => {
+    const token = jwt.sign(Payload, SecretKey, { "expiresIn": "1hr" })
+    return token;
+}
+const VerifyToken = (token) => {
+    return jwt.verify(token, SecretKey)
+}
+
+const Protect = (req, res, next) => {
+    console.log("In protect");
+
+
+    let token = req.headers.authorization?.split(" ")[1];
+    console.log(token);
+
+
+    if (!token) {
+        return res.status(401).json({ Message: "Unauthorized, No Token Provided" });
+    }
+
+    try {
+        const decoded = VerifyToken(token)
+
+        const user = decoded.User;
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(403).json({ Message: error.message });
+    }
+};
+
+module.exports = { GenerateToken, VerifyToken, Protect }
