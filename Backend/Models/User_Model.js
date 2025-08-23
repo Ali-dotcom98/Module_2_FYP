ec8e3afb71d204ce7593f1c6287ac95cfd5b4d81
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-08-23 09:57:52 +0500

    Models Added

---
+++ Backend/Models/User_Model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserModel = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    status: {
+        type: String,
+        required: true,
+        enum: ["Student", "Instructor"],
+        default: "Student"
+    }
+
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("User", UserModel)
