6ad38fc0c5f1c3920daebc9e59d38722b330ab0d
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-08-25 10:17:15 +0500

    Utility added

---
+++ Frontend/Collaboration/src/Utility/ApiPath.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "http://localhost:3000"
+
+export const API_PATH = {
+    AUTH: {
+        LOGIN: "/Auth/Login",
+        REGISTER: "/REGISTER",
+        PROFILE: "/Auth/profile"
+    }
+}
