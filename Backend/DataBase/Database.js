c0c881f57d246b9517e00cb88221f734b42b3ab1
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-08-25 17:03:00 +0500

    Others

--- Backend/DataBase/Database.js
+++ Backend/DataBase/Database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const UserModel = require("../Models/User_Model")
+const AssignmentModel = require("../Models/Assingment_Model")
 const ConnectDB = async () => {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/Collaboration')
--- Backend/Server.js
+++ Backend/Server.js
@@ -2,13 +2,19 @@ const express = require("express")
 const app = express();
 const { ConnectDB } = require("./DataBase/Database.js")
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 ConnectDB();
 
 const AuthRoutes = require("./Router/Auth_Routes.js")
-
+const AssingmentRoutes = require("./Router/Assingment_Routes.js")
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json())
 app.use("/Auth", AuthRoutes)
+app.use("/Assign", AssingmentRoutes)
 
 app.get("/", (req, res) => {
     try {
