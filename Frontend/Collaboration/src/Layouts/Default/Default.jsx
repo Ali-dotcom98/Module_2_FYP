d511869be5b91436f41552cefeb923e7b74f56a2
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-08-25 10:18:58 +0500

    layouts

--- Frontend/Collaboration/src/Layouts/Default/Default.jsx
+++ Frontend/Collaboration/src/Layouts/Default/Default.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
+import SideBar from '../../Components/SideBar/Sidebar'
 const Default = () => {
   return (
-    <div>Default</div>
+    <div className='flex'>
+      <div className='w-[20%] '><SideBar/></div>
+      <div className='w-[80%]'>
+        <Outlet/>
+      </div>
+
+    </div>
   )
 }
 
