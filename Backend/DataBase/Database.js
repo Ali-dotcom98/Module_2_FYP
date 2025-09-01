07f82f99d636f120f1811625d909ed6d3f820b9f
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-09-01 19:23:09 +0500

    Update in Server

--- Backend/DataBase/Database.js
+++ Backend/DataBase/Database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const UserModel = require("../Models/User_Model")
 const AssignmentModel = require("../Models/Assingment_Model")
+const partialSubmissionSchema = require("../Models/PartialSubmission_Model")
 const ConnectDB = async () => {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/Collaboration')
--- Backend/Server.js
+++ Backend/Server.js
@@ -16,21 +16,62 @@ const io = new Server(server, {
         origin: "http://localhost:5173",
     },
 });
+
 io.on("connection", (socket) => {
     console.log(socket.id, "Connected");
-    socket.emit("Welcome", "Welcome Everybody");
-    socket.on("Welcome", ({ name }) => {
-        console.log(`${name} has Joined the Server`);
-        socket.broadcast.emit("Welcome", `${name} has Joined the Server`);
 
+    socket.on("joinGroup", async ({ assignmentId, User }) => {
+        const assignment = await Assingment_Model.findById(assignmentId);
+        if (!assignment) return;
 
+        let groupIndex = null;
+        assignment.settings.groupSettings.groupsDetail.forEach((group, idx) => {
+            if (group.some(student => student._id.toString() === User._id)) {
+                groupIndex = idx;
+            }
+        });
+
+        if (groupIndex === null) return;
+
+        const groupName = `${assignmentId}-group-${groupIndex}`;
+        socket.join(groupName);
+
+        // Tell everyone else that user joined
+        socket.to(groupName).emit("userJoined", {
+            name: User.name,
+            groupId: groupName
+        });
+
+        // Also tell the joining user their group
+        socket.emit("userJoined", {
+            name: User.name,
+            groupId: groupName
+        });
+
+        console.log(`User ${User.name} joined room ${groupName}`);
+
+        const room = io.sockets.adapter.rooms.get(groupName);
+        if (room) {
+            console.log("Users in room:", Array.from(room));
+        } else {
+            console.log("Room is empty or does not exist");
+        }
     });
+
+    socket.on("sendMessage", ({ SocketGroup, User, message }) => {
+        io.to(SocketGroup).emit("receiveMessage", User, message);
+    });
+
+    socket.on("typing", ({ groupName, userName, isTyping }) => {
+        // Notify all other users in the group except the sender
+        socket.to(groupName).emit("userTyping", { userName, isTyping });
+    });
+
 
     socket.on("disconnect", () => {
         console.log(socket.id, "Disconnected");
         socket.broadcast.emit("Welcome", `${socket.id} has Left the Server`);
     });
-
 });
 
 
@@ -41,6 +82,8 @@ io.on("connection", (socket) => {
 const AuthRoutes = require("./Router/Auth_Routes.js")
 const AssingmentRoutes = require("./Router/Assingment_Routes.js");
 const User_Model = require("./Models/User_Model.js");
+const PartialSubmission = require("./Router/PartialSubmission_Route.js");
+const Assingment_Model = require("./Models/Assingment_Model.js");
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
@@ -48,6 +91,7 @@ app.use(cors({
 app.use(express.json())
 app.use("/Auth", AuthRoutes)
 app.use("/Assign", AssingmentRoutes)
+app.use("/Partial", PartialSubmission)
 
 
 app.get('/x/:id', async (req, res) => {
