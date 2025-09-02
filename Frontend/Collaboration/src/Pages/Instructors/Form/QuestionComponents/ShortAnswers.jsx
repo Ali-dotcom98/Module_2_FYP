9e0ce49b38b32b0202c685a38e9134dfcd634fcc
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-09-02 21:21:14 +0500

    Short Answer can be saved  in collaborative environment

--- Frontend/Collaboration/src/Pages/Instructors/Form/QuestionComponents/ShortAnswers.jsx
+++ Frontend/Collaboration/src/Pages/Instructors/Form/QuestionComponents/ShortAnswers.jsx
@@ -1,12 +1,18 @@
 import { Lock, Save, Unlock, X } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react'
 import {UserContext} from '../../../../ContextApi/UserContext'
+import Model from "../../../../Layouts/Modal"
 
-const ShortAnswers = ({item , removeQuestion, index , UpdateItemInArray, type}) => {
+const ShortAnswers = ({item , removeQuestion, index , UpdateItemInArray,updateArrayItem, type , WhoIsAnswering  , DisplayAnswer, DisableQuestionbyIndex  , HandleSave}) => {
     const {User} = useContext(UserContext);
+    const [ConfirmSave, setConfirmSave] = useState(false)
+    console.log("item",User);
+    
+    
   return (
-    <div className=' border border-dashed px-3 py-2  rounded-md'>
+    <>
+        <div className=' border border-dashed border-purple-300 px-3 py-2  rounded-md'>
             <div className="col-span-2 mt-3 ">
                 <div className='flex items-center justify-between'>
                     <label className="text-sm font-medium text-slate-600">
@@ -14,18 +20,28 @@ const ShortAnswers = ({item , removeQuestion, index , UpdateItemInArray, type}) 
                     </label>
                     
                     <div className='flex items-center gap-4 font-urbanist font-semibold'>
-                        {
-                            item?.Lock == true ?(
-                                <div className=' text-sm flex items-center justify-center gap-1  rounded-xl px-3 py-0.5 bg-red-100 text-red-900'>
-                                    <Lock className='size-4'/><p>Close for editing</p>
-                                </div>
-                            ):
-                            (
-                                <div className=' text-sm flex items-center justify-center gap-1  rounded-xl px-3 py-0.5 bg-green-100 text-green-900'>
-                                    <Unlock className='size-4'/>Open for editing
-                                </div>
-                            )
-                        }
+                        
+                        <div>
+                            {
+                                User.status == 'Student' && (
+                                    <div>
+                                        {
+                                        item?.isLocked == true ?(
+                                            <div className=' text-sm flex items-center justify-center gap-1  rounded-xl px-3 py-0.5 bg-red-100 text-red-900'>
+                                                <Lock className='size-4'/><p>Close for editing</p>
+                                            </div>
+                                        ):
+                                        (
+                                            <div className=' text-sm flex items-center justify-center gap-1  rounded-xl px-3 py-0.5 bg-green-100 text-green-900'>
+                                                <Unlock className='size-4'/>Open for editing
+                                            </div>
+                                        )
+                                    }
+                                    </div>
+                                    
+                                )
+                            }
+                        </div>
                         <label className="text-xs font-medium text-slate-600">
                             Marks    
                         </label>
@@ -49,11 +65,15 @@ const ShortAnswers = ({item , removeQuestion, index , UpdateItemInArray, type}) 
                                 </label>
                             </div>
                             <textarea
-                                placeholder="Answer Here"
-                                className={`form-input resize-none ${type ? "h-32":""}`}
+                                placeholder={WhoIsAnswering ? `${WhoIsAnswering?.name} is answering ...  ` : "Answer Here"}
+                                disabled={
+                                    item.isLocked 
+                                    || (WhoIsAnswering && WhoIsAnswering?._id !== User._id && DisableQuestionbyIndex === index) // case 2: someone else is typing
+                                }
+                                className={`w-full text-sm  outline-none border   px-2.5 py-3 rounded-md mt-2 placeholder:text-gray-500 focus-within:border-purple-300 resize-none ${type ? "h-32":""}  ${ WhoIsAnswering && WhoIsAnswering?._id !== User._id && DisableQuestionbyIndex == index ?"border-slate-200 bg-slate-100 text-gray-400 font-medium italic":"border-slate-100 bg-white text-black"}`}
                                 rows={4}
-                                value={item.answer  || ""}
-                                onChange={({ target }) => UpdateItemInArray(index , "answer", target.value)}
+                                value={item.answer}
+                                onChange={({ target }) => updateArrayItem(index , "answer", target.value)}s
                             />
                         </>
                     )
@@ -69,15 +89,62 @@ const ShortAnswers = ({item , removeQuestion, index , UpdateItemInArray, type}) 
                 }
                 {
                     type  && (
-                        <button className='flex items-center justify-end text-sm  w-full'>
-                            <div className='btn-small-light'>
-                                <Save className='size-4'/> Save
-                            </div>
-                        </button>
+                        <div className='flex items-center justify-between w-full'>
+                            {
+                                DisplayAnswer  && DisableQuestionbyIndex == index &&  (
+                                    <div className='w-full'>
+                                        <p className="w-fit text-sm text-gray-500 italic px-2 py-1 bg-gray-100 rounded-md">{WhoIsAnswering.name} is Typing ....</p>
+                                    </div>
+                                )
+                            }
+
+                        <div className="flex items-center justify-end text-sm w-full">
+                            <button
+                                disabled={item.isLocked}
+                                onClick={() => setConfirmSave(true)}
+                                className={`btn-small-light w-fit flex items-center gap-1
+                                ${item.isLocked ? "opacity-50 cursor-not-allowed" : ""}`}
+                            >
+                                <Save className="size-4" /> Save
+                            </button>
+                        </div>
+
+                        </div>
                     )
                 }
             </div>
-    </div>
+        </div>
+        <Model
+            isOpen={ConfirmSave}
+            onClose={() => setConfirmSave(false)}
+            title ={"Confirm Save"}
+            type={"Banner"}
+            >
+            <div className="p-4 font-urbanist h-full">
+                <h2 className="text-lg font-semibold mb-2">Save Assingment Progress</h2>
+                <p className="text-sm text-gray-700 mb-4">
+                Are you sure you want to save this answer?  
+                Once saved, it cannot be rewritten unless others vote to unlock it.
+                </p>
+
+                <div className="flex justify-end space-x-3 border">
+                <button
+                    className="px-4 py-2 rounded-lg bg-gray-200 text-gray-800 hover:bg-gray-300"
+                    onClick={() => setConfirmSave(false)}
+                >
+                    Cancel
+                </button>
+                <button
+                    className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
+                   onClick={() => (setConfirmSave(false), HandleSave())}
+                >
+                    Save Answer
+                </button>
+                </div>
+            </div>
+        </Model>
+
+    </>
   )
 }
 
