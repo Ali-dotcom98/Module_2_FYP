6d28cefc38ab4d2f86ccd0b2b4c9de7ab18d1496
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-09-02 21:22:51 +0500

    Changes in Main Components

--- Frontend/Collaboration/src/Pages/Students/EditAssingment/EditAssingment.jsx
+++ Frontend/Collaboration/src/Pages/Students/EditAssingment/EditAssingment.jsx
@@ -11,7 +11,7 @@ import {UserContext} from "../../../ContextApi/UserContext"
 import { SocketContext } from '../../../ContextApi/SocketContext';
 import DisplayQuestion from '../Components/DisplayQuestion';
 import axios from 'axios';
-import { Send, Vote } from 'lucide-react';
+import { Send, Slice, Vote } from 'lucide-react';
 
 const EditAssingment = () => {
     const socket = useContext(SocketContext);
@@ -28,6 +28,11 @@ const EditAssingment = () => {
     const [Join, setJoin] = useState("")
     const [messages, setMessages] = useState([]);
     const [text, settext] = useState("")
+    const [typingUser, settypingUser] = useState("")
+    const [WhoIsAnswering, setWhoIsAnswering] = useState("")
+    const [DisplayAnswer, setDisplayAnswer] = useState(false)
+    const [displayTyping, setdisplayTyping] = useState(false)
+    const [DisableQuestionbyIndex, setDisableQuestionbyIndex] = useState(null)
 
     const [DefaultInfo, setDefaultInfo] = useState({
             title : "",
@@ -75,6 +80,7 @@ const EditAssingment = () => {
             {
             name: "",
             status: "",
+            online : false,
             }
         ],
         });
@@ -151,7 +157,10 @@ const EditAssingment = () => {
                     ...q,
                     vote: q.vote || ""  
                 })),
-                Students: Isexist.data.Students || PartialSubmission.Students, 
+                Students: (Isexist.data.Students || PartialSubmission.Students).map(q =>({
+                    ...q ,
+                    online : q.online || false
+                }))     
                 }));
 
             
@@ -172,9 +181,9 @@ const EditAssingment = () => {
     useEffect(() => {
     socket.emit("joinGroup", { assignmentId: AssingmentId, User: User });
 
-    const handleUserJoined = ({ name, groupId }) => {
+    const handleUserJoined = ({ name, groupId , id }) => {
         setSocketGroup(groupId);
-        setJoin(name);
+        setJoin(name);        
     };
 
     const handleReceiveMessage = (User, text) => {
@@ -184,7 +193,43 @@ const EditAssingment = () => {
         ]);
     };
 
+    socket.on("userTyping", (User , Flag)=>{
+        settypingUser(User.name)
+        setdisplayTyping(Flag)
+    })
+
+    socket.on("updateOnlineStatus", (onlineUserIds) => {
+        setPartialSubmission(prev => ({
+            ...prev,
+            Students: prev.Students.map(student => ({
+            ...student,
+            online: onlineUserIds.includes(student._id)
+            }))
+        }));
+        });
+
+    socket.on("Answering", (User , CurrentQuestion ,answer,  Flag)=>{
+        setDisplayAnswer(Flag)
+        setWhoIsAnswering(User)
+        setDisableQuestionbyIndex(CurrentQuestion)
+        setPartialSubmission((prev)=>{
+            const updateArray = [...prev.Questions]
+            const key = "answer"
+            updateArray[CurrentQuestion]={
+                ...updateArray[CurrentQuestion],
+                [key] : answer
+            }
+            return(
+                {
+                    ...prev , 
+                    Questions: updateArray,
+                }
+            )
+        })
+
+    })
     
+
 
     socket.on("userJoined", handleUserJoined);
     socket.on("receiveMessage", handleReceiveMessage);
@@ -192,6 +237,8 @@ const EditAssingment = () => {
     return () => {
         socket.off("userJoined", handleUserJoined);
         socket.off("receiveMessage", handleReceiveMessage);
+        socket.off("userTyping")
+        socket.off("updateOnlineStatus")
     };
 }, [AssingmentId, User]);
 
@@ -207,8 +254,27 @@ const EditAssingment = () => {
         });
             
         settext("");
+        socket.emit("TypingFinish", SocketGroup,User , false)
     }
 
+    const HandleSave = ()=>{
+        setPartialSubmission((prev)=>{
+            const updateArray = [...prev.Questions];
+
+            updateArray[currentIndex] ={
+                ...updateArray[currentIndex],
+                isLocked : true,
+                lockedby : User
+            }
+            return (
+                {
+                    ...prev ,
+                    Questions : updateArray
+                }
+            )
+        })
+
+    }
 
 const NextQuestion = ()=>{
     
@@ -249,25 +315,36 @@ const removeArrayItem = (section, index) => {
   });
 }; 
 
-const updateArrayItem = (section , index ,key , value)=>{
-    setDefaultChlng((prev)=>{
-        const updateArray= [...prev[section]]
-        if(key==null)
-        {
-            updateArray[index]= value
+const updateArrayItem = (index, key, value) => {
+    setPartialSubmission((prev) => {
+        const updateArray = [...prev.Questions];
+
+        if (key == null) {
+        updateArray[index] = value;
+        } else {
+        updateArray[index] = {
+            ...updateArray[index],
+            [key]: value,
+        };
         }
-        else{
-            updateArray[index] = {
-                ...updateArray[index],
-                [key] : value
-            }
+
+        const newState = {
+        ...prev,
+        Questions: updateArray,
+        };
+
+        const answerText = updateArray[currentIndex].answer || "";
+        // check after update
+        if (answerText.trim() === "") {
+        socket.emit("Answering", null, SocketGroup, currentIndex, "", false);
+        } else {
+        socket.emit("Answering", User, SocketGroup, currentIndex,answerText, true);
         }
-        return{
-            ...prev,
-            [section]: updateArray
-        }
-    })
-}
+
+        return newState;
+    });
+};
+
 
 
 const updateSection = (key , value)=>{
@@ -307,26 +384,45 @@ const gotoHome = ()=>{
     navigator("/Student");
 }
 
+const handletext =(e)=>{
+    const {name , value} = e.target
+    settext(e.target.value)
+    socket.emit("Typing", SocketGroup,User, true)
+   
+}
+const handleBlur = () => {
+  socket.emit("Typing", SocketGroup, User, false);
+};
+
 
     return (
     <div className=" px-5 py-4 font-urbanist flex gap-3 ">
-        <div className="hidden md:flex flex-col  items-center justify-between gap-5 bg-white rounded-lg border border-purple-100 py-3 px-4">
+        <div className="hidden md:flex flex-col  items-center justify-between gap-5 bg-white rounded-lg border border-purple-200 py-3 px-4">
         
-            <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-col items-center justify-between h-full gap-4">
                 <button
                 className="btn-small-light"
                 onClick={gotoHome}
                 >
                 <LuArrowLeft className="text-[16px]" />
                 </button>
+                <div className='flex gap-2 flex-col'>
+                    {PartialSubmission.Students.map((item, index) => (
+                        <div className='border rounded-full p-1 relative '>                    
+                            <div key={index} className='border p-2 flex items-center justify-center rounded-full peer'>{item.name.slice(0, 2)}</div>
+                            <div className={`absolute size-3  -translate-y-2 rounded-full ${item.online ? "bg-green-500": "bg-red-500"}`}></div>
+                            <div className='bg-purple-200 px-3 py-1 text-sm absolute -translate-y-10 translate-x-14 rounded-lg z-30 opacity-0 peer-hover:opacity-100'>{item.name}</div>
+                        </div>
+                    ))}
 
+                </div>
             </div>
             
             
         </div>
         <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-1">
-            <div className="h-[95vh]  bg-white col-span-2 rounded-lg border border-purple-100 overflow-hidden relative">
-                <div className="flex items-center justify-between gap-5 bg-white rounded-lg border border-purple-100 py-3 px-4  my-3 mx-2">
+            <div className="h-[95vh]  bg-white col-span-2 rounded-lg border border-purple-200 overflow-hidden relative">
+                <div className="flex items-center justify-between gap-5 bg-white rounded-lg border border-purple-300 py-3 px-4  my-3 mx-2">
                     <h1>{DefaultInfo.title}</h1>
                     <div>
                         <p>0/{DefaultInfo.questions.length}</p>
@@ -339,17 +435,22 @@ const gotoHome = ()=>{
                             <DisplayQuestion
                                 item={PartialSubmission.Questions[currentIndex]}
                                 index = {currentIndex}
+                                updateArrayItem = {updateArrayItem}
+                                WhoIsAnswering = {WhoIsAnswering}
+                                DisplayAnswer ={DisplayAnswer}
+                                DisableQuestionbyIndex ={DisableQuestionbyIndex}
+                                HandleSave ={HandleSave}
                             />
                             )}
                     </div>
                     <div className='row-span-1 -translate-y-3 '>
                         {PartialSubmission.Questions[currentIndex]?.isLocked && (
-                            <div className='border border-dashed rounded-md border-purple-200 flex items-center justify-between  px-3 gap-3 py-2'>
+                            <div className='border border-dashed rounded-md border-purple-300 flex items-center justify-between  px-3 gap-3 py-2'>
                                 
                                 <div className='w-full flex flex-col p-1 '>
                                     <p className="text-sm font-medium text-slate-600">Refinement Votes:</p>
                                     <div className='flex items-center gap-4'>
-                                         <div             
+                                        <div             
                                             className={ `h-0.5 w-full bg-gradient-to-r from-purple-500 to-purple-700 transition-all duration-300`}
                                             style={{width : `${100}%`}}>
 
@@ -418,27 +519,34 @@ const gotoHome = ()=>{
                         {item.message}
                     </div>
                     ))}
+                    {displayTyping && (
+                        <div className="text-sm text-gray-500 italic px-2 py-1 bg-gray-100 rounded-md w-max">
+                            {typingUser} is typing...
+                        </div>
+                        )}
+
                 </div>
 
-  <form
-    className="relative p-3 bg-white border-t border-gray-300"
-    onSubmit={handleSubmit}
-  >
-    <input
-      className="w-full text-sm border border-purple-300 text-black outline-none px-3 py-2 rounded-md placeholder-gray-500 focus:border-purple-500"
-      type="text"
-      value={text}
-      onChange={({ target }) => settext(target.value)}
-      placeholder="Type a message..."
-    />
-    <button
-      type="submit"
-      className="absolute right-4 top-1/2 -translate-y-1/2 p-1 rounded-md text-purple-800"
-    >
-      <Send className="w-5 h-5" />
-    </button>
-  </form>
-</div>
+                <form
+                    className="relative p-3 bg-white border-t border-gray-300"
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                    className="w-full text-sm border border-purple-300 text-black outline-none px-3 py-2 rounded-md placeholder-gray-500 focus:border-purple-500"
+                    type="text"
+                    value={text}
+                    onChange={handletext}
+                    onBlur={handleBlur}
+                    placeholder="Type a message..."
+                    />
+                    <button
+                    type="submit"
+                    className="absolute right-4 top-1/2 -translate-y-1/2 p-1 rounded-md text-purple-800"
+                    >
+                    <Send className="w-5 h-5" />
+                    </button>
+                </form>
+                </div>
 
         </div>
         <Modal
