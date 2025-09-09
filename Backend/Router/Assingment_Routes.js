2e8c1358862789a54d0f02326e1e3ed56f5af936
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-09-09 19:56:05 +0500

    Update in Routers

--- Backend/Router/Assingment_Routes.js
+++ Backend/Router/Assingment_Routes.js
@@ -3,8 +3,10 @@ const { Protect } = require("../Middleware/TokenMiddleware");
 const Assingment_Model = require("../Models/Assingment_Model");
 const User_Model = require("../Models/User_Model");
 const PartialSubmission_Model = require("../Models/PartialSubmission_Model");
+const upload = require("../Middleware/Upload_Middleware");
 const route = express.Router();
-
+const path = require("path");
+const fs = require("fs")
 route.post("/Create", Protect, async (req, res) => {
     try {
         const { title } = req.body
@@ -12,6 +14,7 @@ route.post("/Create", Protect, async (req, res) => {
             description: "",
             dueDate: "",
             totalMarks: 0,
+            thumbnail: "",
             questions: [
                 {
                     questionText: "",
@@ -73,16 +76,36 @@ route.put("/Update/:id", Protect, async (req, res) => {
         res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
 })
+// GET /Assingments?page=1&limit=10
 route.get("/Assingments", Protect, async (req, res) => {
     try {
-        const Assingments = await Assingment_Model.find({
-            Instructor: req?.user?._id
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const total = await Assingment_Model.countDocuments({
+            Instructor: req?.user?._id,
+        });
+
+        const assingments = await Assingment_Model.find({
+            Instructor: req?.user?._id,
         })
-        res.send(Assingments)
+            .skip(skip)
+            .limit(limit)
+            .sort({ createdAt: -1 });
+
+        res.json({
+            page,
+            limit,
+            totalItems: total,
+            totalPages: Math.ceil(total / limit),
+            data: assingments,
+        });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-})
+});
+
 route.get("/Assingments/:id", Protect, async (req, res) => {
     try {
         const Assingments = await Assingment_Model.findOne({
@@ -163,6 +186,95 @@ route.get("/student", Protect, async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+route.put("/:id/upload-image", (req, res) => {
+    try {
+        upload.fields([{ name: 'thumbnail' },])(req, res, async (err) => {
+            if (err) {
+                return res.status(400).json({ message: "File upload failed", error: err.message });
+            }
+
+            const AssingmentId = req.params.id;
+            const Assignment = await Assingment_Model.findOne({ _id: AssingmentId });
+            console.log(Assignment);
+
+
+            if (!Assignment) {
+                return res.status(404).json({ message: "Assignment not found or unauthorized" });
+            }
+
+            const uploadsFolder = path.join(__dirname, '..', 'upload');
+            console.log("uploadsFolder", uploadsFolder);
+
+            const baseUrl = `${req.protocol}://${req.get("host")}`;
+
+            const newThumbnail = req.files.thumbnail?.[0];
+
+
+            if (newThumbnail) {
+                if (Assignment.thumbnail) {
+                    const oldThumbnail = path.join(uploadsFolder, path.basename(Assignment.thumbnail));
+                    if (fs.existsSync(oldThumbnail)) fs.unlinkSync(oldThumbnail);
+                }
+                Assignment.thumbnail = `${baseUrl}/uploads/${newThumbnail.filename}`;
+            }
+
+
+            await Assignment.save();
+            res.status(200).json({
+                Message: "Images uploaded Successfully",
+                thumbnail: Assignment.thumbnail,
+            })
+        });
+    } catch (error) {
+        console.error("Error in uploadResumeImages:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+
+route.delete("/Assingments/:id", Protect, async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const deleted = await Assingment_Model.findByIdAndDelete(id);
+
+        if (!deleted) {
+            return res.status(404).json({
+                success: false,
+                message: "Assignment not found",
+            });
+        }
+        res.json({
+            success: true,
+            message: "Assignment deleted successfully",
+            deletedId: id,
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+});
+route.get("/Assingments/Count/By-day", Protect, async (req, res) => {
+    try {
+        const stats = await Assingment_Model.aggregate([
+            {
+                $group: {
+                    _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                    count: { $sum: 1 }
+                }
+            },
+            { $sort: { _id: 1 } }
+        ]);
+
+        res.json(stats);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
 
 
 
