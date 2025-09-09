8f4cf8c33646d3b7ed2319feb3eb78a57bd70641
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-09-09 19:51:08 +0500

    Components Added

---
+++ Frontend/Collaboration/src/Components/Cards/DeleteCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const DeleteCard = ({AssingmentInfo, HandleDelete}) => {
+  return (
+    <div className=''>
+      <div className="font-urbanist text-black px-6 space-y-5">
+          <div className='space-y-3'>
+            <h3 className="text-lg font-semibold text-black">Delete Current Assingment</h3>
+            <p className="text-md text-center ">
+              You're about to permanently delete the  titled{' '}
+              <span className="font-semibold text-red-600">"{AssingmentInfo.title}"</span>.
+              
+          </p>
+          <p className="text-xs text-slate-700 mt-[5px] ">
+              This action cannot be undone.
+          </p>
+          </div>
+          <div className="flex items-center justify-center  gap-4">
+              <button
+                  onClick={HandleDelete}
+                  className="btn-Delete"
+              >
+                  Yes, Delete
+              </button>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default DeleteCard
